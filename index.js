23f4a7a1581e3695c5201206aead468bf529b465
Daniel Shlomo <dan.s@rvconnex.com>
2021-06-17 20:25:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
          * @param  {String} server The options for the client (Like server)
          */
         constructor(server) {
-            if(!server) throw new Error('You must specify the server\'s URL')
+            if (!server) throw new Error('You must specify the server\'s URL')
             this.server = server;
         }
 
@@ -46,9 +46,15 @@ module.exports = {
              * @param  {Function} callback
              */
             this.process = async (body, callback) => {
-                const { args, fields, model } = body;
+                const {
+                    args,
+                    fields,
+                    model
+                } = body;
                 if (!model || !this.models[model]) {
-                    callback({ error: 'Method doesn\'t exists' }, null)
+                    callback({
+                        error: 'Method doesn\'t exists'
+                    }, null)
                 } else {
                     if (!args) args = {};
 
@@ -56,7 +62,9 @@ module.exports = {
                     if (this.models[model].schema) validation = validate(args, this.models[model].schema);
 
                     if (validation && validation.errors.length > 0) {
-                        callback({ errors: validation.errors }, null);
+                        callback({
+                            errors: validation.errors
+                        }, null);
                     } else {
                         const result = await this.models[model].method(args)
                         callback(null, this.resolveFields(result, fields))
@@ -76,17 +84,23 @@ module.exports = {
 
         resolveFields(result, fields) {
             if (fields) {
-                for (const key in result) {
-                    if (!fields[key]) {
-                        delete result[key]
-                    } else {
-                        if (this.objSize(result[key]) > 0 && fields[key] != '*') result[key] = this.resolveFields(result[key], fields[key])
+                if (Array.isArray(fields)) {
+                    for (let i = 0; i < result.length; i++) {
+                        result[i] = this.resolveFields(result[i], fields[0])
+                    }
+                } else {
+                    for (const key in result) {
+                        if (!fields[key]) {
+                            delete result[key]
+                        } else {
+                            if (this.objSize(result[key]) > 0 && fields[key] != '*') result[key] = this.resolveFields(result[key], fields[key])
+                        }
                     }
                 }
             }
             return result
         }
-        
+
         /**
          * Resolve a model
          * @param  {Object} model
